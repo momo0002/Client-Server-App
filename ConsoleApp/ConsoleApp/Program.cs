9d8bf18dc2b50fe65f0e2d54d30c2691605d90e7
Moe <info.mortada@gmail.com>
2015-12-03 22:45:57 +0100

    Today

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    //http://stackoverflow.com/questions/10200910/create-hello-world-websocket-example
+    //https://msdn.microsoft.com/en-us/library/debx8sh9(v=vs.110).aspx
     class Program
     {
         static void Main(string[] args)
@@ -15,45 +17,94 @@ namespace ConsoleApp
 
         static async Task RunAsync()
         {
-            string line = "";
-            do
+            string line = null;
+            while((line = Console.ReadLine()) != null)
             {
                 Console.Write("Enter Message: ");
-                line = Console.ReadLine();
-
-                if (line != "")
+                using (var client = new HttpClient())
                 {
-                    using (var client = new HttpClient())
+                    client.BaseAddress = new Uri("http://localhost:23373/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // HTTP POST
+                    //var msg = new Messages() { message = "test", time = "time" };
+
+                    var content = new FormUrlEncodedContent(new[]{
+                        new KeyValuePair<string, string>("message", line),
+                        new KeyValuePair<string, string>("time", DateTime.Now.ToString()),
+                    });
+
+                    try
                     {
-                        client.BaseAddress = new Uri("http://localhost:23373/");
-                        client.DefaultRequestHeaders.Accept.Clear();
-                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                        //HttpResponseMessage response = await client.GetAsync("Home/Index");
-
-                        // HTTP POST
-                        //var msg = new Messages() { message = "test", time = "time" };
-
-                        var content = new FormUrlEncodedContent(new[]{
-                            new KeyValuePair<string, string>("message", line),
-                            new KeyValuePair<string, string>("time", DateTime.Now.ToString()),
+                        //HttpResponseMessage response = await client.PostAsync("Home/Index", content);
+                        await client.PostAsync("Home/Index", content).ContinueWith(task =>
+                        {
+                            var response = task.Result;
+                            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
                         });
 
-                        try
-                        {
-                            HttpResponseMessage response = await client.PostAsync("Home/Index", content);
-                            response.EnsureSuccessStatusCode();
-
-                            //var responseString = await response.Content.ReadAsStringAsync();
-
-                        }
-                        catch (HttpRequestException e)
-                        {
-                            Console.WriteLine("Error" + e.GetBaseException());
-                        }
+                        //var responseString = await response.Content.ReadAsStringAsync();
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine("Error" + e.GetBaseException());
                     }
                 }
-            } while (line != "");           
+            }
         }
     }
 }
+
+
+
+
+
+
+    /*
+static void Main(string[] args)
+{
+    //Createa webrequest
+    WebRequest request = WebRequest.Create("http://localhost:23373/Home/Index");
+
+    // Optional in this proj
+    // setup Credentials
+    request.Credentials = CredentialCache.DefaultCredentials;
+
+    //Proprety of request
+    request.Method = "POST";
+
+    string line = null;
+    while ((line = System.Console.ReadLine()) != null)
+    {
+        string postData = ("&message=" + line + "&time=" + DateTime.Now);
+        byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+        // Set the ContentType property of the WebRequest.
+        request.ContentType = "application/x-www-form-urlencoded";
+        // Set the ContentLength property of the WebRequest.
+        request.ContentLength = byteArray.Length;
+        // Get the request stream.
+        Stream dataStream = request.GetRequestStream();
+        // Write the data to the request stream.
+        dataStream.Write(byteArray, 0, byteArray.Length);
+        // Close the Stream object.
+        dataStream.Close();
+        // Get the response.
+        WebResponse response = request.GetResponse();
+        // Display the status.
+        Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+        // Get the stream containing content returned by the server.
+        dataStream = response.GetResponseStream();
+        // Open the stream using a StreamReader for easy access.
+        StreamReader reader = new StreamReader(dataStream);
+        // Read the content.
+        string responseFromServer = reader.ReadToEnd();
+        // Display the content.
+        Console.WriteLine(responseFromServer);
+        // Clean up the streams.
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+    }
+    Console.ReadLine();
+}*/
---
+++ ConsoleApp/WebApp/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(WebApp.App_Start.Startup))]
+
+namespace WebApp.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
